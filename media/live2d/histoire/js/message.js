0a9eafa7bd1437c74ccf6bde39a5069b7166a4e4
hsxyhao <806649821@qq.com>
2020-03-09 01:58:21 +0800

    update from gridea: 2020-03-09 01:58:21

--- media/live2d/histoire/js/message.js
+++ media/live2d/histoire/js/message.js
@@ -437,10 +437,10 @@ if(!norunFlag){
 	}
 	$(document).ready(function() {
 		var AIimgSrc = [
-			home_Path + message_Path + "model/histoire/histoire.1024/texture_00.png",
-			home_Path + message_Path + "model/histoire/histoire.1024/texture_01.png",
-			home_Path + message_Path + "model/histoire/histoire.1024/texture_02.png",
-			home_Path + message_Path + "model/histoire/histoire.1024/texture_03.png"
+			message_Path + "model/histoire/histoire.1024/texture_00.png",
+			message_Path + "model/histoire/histoire.1024/texture_01.png",
+			message_Path + "model/histoire/histoire.1024/texture_02.png",
+			message_Path + "model/histoire/histoire.1024/texture_03.png"
 		]
 		var images = [];
 		var imgLength = AIimgSrc.length;
